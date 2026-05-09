90cd84f414756747e8a1314f9ccc44453f17af27
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-05-09 18:22:03 +0200

    feat: basic window and background

---
+++ MadEngine/Game.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+namespace MadEngine;
+
+public class Game : GameWindow
+{
+    public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings()
+    {
+        ClientSize = (width, height), Title = title 
+        
+    }) { }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1f);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        
+        SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+}
---
+++ MadEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MadEngine;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Game game = new Game(800, 600, "Mad Engine"))
+        {
+            game.Run();
+        }
+    }
+}
