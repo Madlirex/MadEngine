16a0a64e208c2290e804eeb318b594e61916bb2f
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-05-09 18:39:46 +0200

    feat: add buffer binding and basic shape

--- MadEngine/Game.cs
+++ MadEngine/Game.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿using System.ComponentModel;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -8,6 +9,9 @@ namespace MadEngine;
 
 public class Game : GameWindow
 {
+    private int VertexBufferObject;
+    private Shape Triangle = new Shape([-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f]);
+    
     public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings()
     {
         ClientSize = (width, height), Title = title 
@@ -18,7 +22,19 @@ public class Game : GameWindow
     {
         base.OnLoad();
         
+        VertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, Triangle.Vertices.Length * sizeof(float), Triangle.Vertices, BufferUsageHint.StaticDraw);
+        
         GL.ClearColor(0.2f, 0.3f, 0.3f, 1f);
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        base.OnClosing(e);
+        
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        GL.DeleteBuffer(VertexBufferObject);
     }
 
     protected override void OnRenderFrame(FrameEventArgs args)
@@ -44,6 +60,6 @@ public class Game : GameWindow
     {
         base.OnFramebufferResize(e);
         
-        GL.Viewport(0f, 0f, e.Width, e.Height);
+        GL.Viewport(0, 0, e.Width, e.Height);
     }
 }
