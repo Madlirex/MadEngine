084265761319dd1c744ad47425780cb43ea16823
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-05-09 18:54:23 +0200

    feat(shader): shaders compiling

---
+++ MadEngine/Shader.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace MadEngine;
+
+public class Shader
+{
+    private int Handle;
+
+    public Shader(string vertexPath, string fragmentPath)
+    {
+        string vertexShaderSource = File.ReadAllText(vertexPath);
+        string fragmentShaderSource = File.ReadAllText(fragmentPath);
+        
+        int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(vertexShader, vertexShaderSource);
+        
+        int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(fragmentShader, fragmentShaderSource);
+        
+        GL.CompileShader(vertexShader);
+
+        GL.GetShader(vertexShader, ShaderParameter.CompileStatus, out int success);
+        if (success == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(vertexShader);
+            Console.WriteLine(infoLog);
+        }
+
+        GL.CompileShader(fragmentShader);
+
+        GL.GetShader(fragmentShader, ShaderParameter.CompileStatus, out success);
+        if (success == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(fragmentShader);
+            Console.WriteLine(infoLog);
+        }
+    }
+}
