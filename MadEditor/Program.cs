76d83bc0cb3f5f49366e4d9b357d295ceb768661
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-05-31 18:55:18 +0200

    feat(runtime): change export type to WinExe + BUILDS

--- MadEditor/Program.cs
+++ MadEditor/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        using (EditorWindow game = new EditorWindow(1920, 1080, "Mad Engine"))
+        using (EditorWindow game = new EditorWindow(1200, 800, "Mad Engine"))
         {
             game.Run();
         }
