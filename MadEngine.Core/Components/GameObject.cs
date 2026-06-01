b1e1eff919c3c359cce210aa780e51449e070c11
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-06-01 18:12:33 +0200

    feat(gameobject): add finding helpers

--- MadEngine.Core/Components/GameObject.cs
+++ MadEngine.Core/Components/GameObject.cs
@@ -1,4 +1,6 @@
-﻿namespace MadEngine.Core;
+﻿using MadEngine.Core.SceneManagement;
+
+namespace MadEngine.Core;
 
 public class GameObject
 {
@@ -45,5 +47,83 @@ public class GameObject
     public T? GetComponent<T>() where T : Component
     {
         return Components.OfType<T>().FirstOrDefault();
+    }
+
+    public T[] GetComponents<T>() where T : Component
+    {
+        return Components.OfType<T>().ToArray();
+    }
+
+    public static GameObject? FindByName(string name)
+    {
+        foreach (GameObject obj in SceneManager.ActiveScene.GameObjects)
+        {
+            if (obj.Name == name) return obj;
+        }
+
+        return null;
+    }
+    
+    public static GameObject[] FindAllByName(string name)
+    {
+        List<GameObject> objs = [];
+        foreach (GameObject obj in SceneManager.ActiveScene.GameObjects)
+        {
+            if (obj.Name == name) objs.Add(obj);
+        }
+
+        return objs.ToArray();
+    }
+    
+    public static GameObject? FindFirstByComponent<T>() where T : Component
+    {
+        foreach (GameObject obj in SceneManager.ActiveScene.GameObjects)
+        {
+            if (obj.GetComponent<T>() != null)
+            {
+                return obj;
+            }
+        }
+
+        return null;
+    }
+
+    public static GameObject[] FindAllByComponent<T>() where T : Component
+    {
+        List<GameObject> objs = [];
+        foreach (GameObject obj in SceneManager.ActiveScene.GameObjects)
+        {
+            if (obj.GetComponent<T>() != null)
+            {
+                objs.Add(obj);
+            }
+        }
+
+        return objs.ToArray();
+    }
+
+    public static T? FindFirstComponent<T>() where T : Component
+    {
+        foreach (GameObject obj in SceneManager.ActiveScene.GameObjects)
+        {
+            T? comp = obj.GetComponent<T>();
+            if (comp != null)
+            {
+                return comp;
+            }
+        }
+
+        return null;
+    }
+
+    public static T[] FindAllComponents<T>() where T : Component
+    {
+        List<T> components = [];
+        foreach (GameObject obj in  SceneManager.ActiveScene.GameObjects)
+        {
+            components.AddRange(obj.GetComponents<T>());
+        }
+
+        return components.ToArray();
     }
 }
