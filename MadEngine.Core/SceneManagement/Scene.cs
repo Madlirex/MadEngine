4c55303922d3e57b1a143fe6c95335bb3bb1d1a5
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-06-02 15:37:46 +0200

    feat(scene): implement new registration system

--- MadEngine.Core/SceneManagement/Scene.cs
+++ MadEngine.Core/SceneManagement/Scene.cs
@@ -2,13 +2,18 @@
 
 public class Scene
 {
-    public readonly List<GameObject> GameObjects = [];
-    public List<Light> Lights = [];
-    public List<MeshRenderer> MeshRenderers = [];
+    private readonly List<GameObject> _gameObjects = [];
+    public IReadOnlyList<GameObject> GameObjects => _gameObjects;
+
+    private readonly List<Light> _lights = [];
+    public IReadOnlyList<Light> Lights => _lights;
+
+    private readonly List<MeshRenderer> _meshRenderers = [];
+    public IReadOnlyList<MeshRenderer> MeshRenderers => _meshRenderers;
 
     public void Add(GameObject gameObject)
     {
-        GameObjects.Add(gameObject);
+        _gameObjects.Add(gameObject);
         Register(gameObject);
     }
 
@@ -18,19 +23,35 @@ public class Scene
         {
             RegisterComponent(component);
         }
+
+        gameObject.ComponentAdded += RegisterComponent;
+        gameObject.ComponentRemoved += UnregisterComponent;
     }
 
     public void RegisterComponent(Component component)
     {
         if (component is Light light)
         {
-            Lights.Add(light);
+            _lights.Add(light);
         }
 
         if (component is MeshRenderer meshRenderer)
         {
-            MeshRenderers.Add(meshRenderer);
+            _meshRenderers.Add(meshRenderer);
             meshRenderer.Mesh.Initialize();
+        }
+    }
+
+    public void UnregisterComponent(Component component)
+    {
+        if (component is Light light)
+        {
+            _lights.Remove(light);
+        }
+
+        if (component is MeshRenderer meshRenderer)
+        {
+            _meshRenderers.Remove(meshRenderer);
         }
     }
 }
