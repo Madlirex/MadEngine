cb3c34d18603654ddf2ab2195ad56f88f52ff826
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-06-02 15:54:06 +0200

    feat(core): rules for components such as add or remove

---
+++ MadEngine.Core/ComponentRules.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace MadEngine.Core;
+
+public static class ComponentRules
+{
+    public static bool CanBeAdded(Type type)
+    {
+        var attr = type.GetCustomAttribute<CanBeAddedAttribute>();
+
+        return attr?.Value ?? true;
+    }
+
+    public static bool CanBeRemoved(Type type)
+    {
+        var attr = type.GetCustomAttribute<CanBeRemovedAttribute>();
+
+        return attr?.Value ?? true;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class CanBeAddedAttribute : Attribute
+{
+    public bool Value;
+
+    public CanBeAddedAttribute(bool value)
+    {
+        Value = value;
+    }
+
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class CanBeRemovedAttribute : Attribute
+{
+    public bool Value;
+
+    public CanBeRemovedAttribute(bool value)
+    {
+        Value = value;
+    }
+
+}
