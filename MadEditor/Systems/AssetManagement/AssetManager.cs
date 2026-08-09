42cbc5b6c1a0b48bbf55f02beca9bec20395e013
Madlirex <simon.buzgo@gmail.com>
2026-08-09 18:21:42 +0200

    feat(asset-management): redo

--- MadEditor/Systems/AssetManagement/AssetManager.cs
+++ MadEditor/Systems/AssetManagement/AssetManager.cs
@@ -1,137 +1,32 @@
 ﻿using MadEngine.Core;
-using MadEngine.Core.SceneManagement;
 
 namespace MadEditor;
 
 public static class AssetManager
 {
-    public static string ProjectPath => _projectPath;
-    public static string AssetsPath => _projectPath + @"\Assets\";
-    public static string PackagesPath => _projectPath + @"\Packages\";
-    private static string _projectPath = "";
+    public static IReadOnlyDictionary<Guid, MadObject> Objects => _objects;
+    private static Dictionary<Guid, MadObject> _objects = new();
 
-    private static Dictionary<string, IAssetImporter> _importers = new();
-    private static Dictionary<string, IAssetImporter> _importersByExtension = new();
-
-    public static void SetPath(string path)
+    public static void AddObject(MadObject obj)
     {
-        _projectPath = path;
-    }
-    
-    public static void LoadProject(string path)
-    {
-        DiscoverImporters();
-        SerializerRegistry.DiscoverSerializers();
-        InstantiateAssets(path);
-        LoadAssets(AssetRegistry.Assets);
-        Console.WriteLine(SceneManager.Scenes.Count);
-    }
-
-    public static void SaveProject()
-    {
-        SaveAssets();
-    }
-    
-    public static void RecompileScripts()
-    {
-        if (Directory.Exists(ProjectPath))
-        {
-            var scriptFiles = Directory.GetFiles(ProjectPath, "*.cs", SearchOption.AllDirectories);
-
-            ScriptDomain.ReloadFromFiles(scriptFiles);
-        }
-    }
-    
-    public static void InstantiateAssets(string path)
-    {
-        if (Directory.Exists(path))
-        {
-            var files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories);
-            foreach (var file in files)
-            {
-                InstantiateAsset(file);
-            }
-        }
-    }
-
-    public static void InstantiateAsset(string path)
-    {
-        Asset asset;
-        if (File.Exists(path + ".meta"))
-        {
-            AssetMeta meta = MetaUtility.Load(path + ".meta");
-            meta.Path = Application.Directory + meta.Path; 
-            asset = _importers[meta.Importer].Instantiate(meta);
-            asset.FullDir = Path.GetDirectoryName(path) ?? "";
-        }
+        if (!_objects.ContainsKey(obj.Guid))
+            _objects.Add(obj.Guid, obj);
         else
+            Console.WriteLine("WARNING: Duplicate object: " + obj.Guid);
+    }
+
+    public static void RemoveObject(Guid guid)
+    {
+        if (!_objects.ContainsKey(guid))
         {
-            if (_importersByExtension.TryGetValue(Path.GetExtension(path), out var importer))
-            {
-                asset = importer.Instantiate(path);
-                asset.FullDir = Path.GetDirectoryName(path) ?? "";
-            }
+            Console.WriteLine("ERROR: Object not found: " + guid);
+            return;
         }
+        _objects.Remove(guid);
     }
 
-    public static void LoadAssets(Asset[] assets)
+    public static void RemoveObject(MadObject obj)
     {
-        foreach (Asset asset in assets)
-        {
-            LoadAsset(asset);
-        }
-    }
-    
-    public static void LoadAsset(Asset asset)
-    {
-        if (_importersByExtension.TryGetValue(Path.GetExtension(asset.AbsolutePath), out var importer))
-        {
-            importer.Load(asset);
-        }
-    }
-
-    public static void SaveAssets()
-    {
-        foreach (var asset in AssetRegistry.Assets)
-        {
-            SaveAsset(asset, asset.AbsolutePath);
-        }
-    }
-
-    public static void SaveAsset(Asset asset, string path)
-    {
-        Console.WriteLine(path);
-        if (!_importersByExtension.TryGetValue(Path.GetExtension(path), out var importer)) return;
-        importer.Save(asset, path);
-
-        AssetMeta meta = MetaUtility.GenerateMeta(asset);
-        
-        MetaUtility.Save(path + ".meta", meta);
-    }
-
-    public static void DiscoverImporters()
-    {
-        _importers.Clear();
-        _importersByExtension.Clear();
-        
-        var importers = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(assembly => assembly.GetTypes())
-            .Where(type => typeof(IAssetImporter).IsAssignableFrom(type) && type is { IsAbstract: false, IsInterface: false });
-        foreach(Type importer in importers)
-        {
-            IAssetImporter importerInstance = (IAssetImporter)Activator.CreateInstance(importer)!;
-            
-            _importers.Add(importerInstance.Name, importerInstance);
-            
-            foreach (string extension in importerInstance.Extensions)
-            {
-                _importersByExtension.Add(extension, importerInstance);
-            }
-        }
-    }
-
-    public static bool TryGetImporter(string extension, out IAssetImporter? importer)
-    {
-        return _importersByExtension.TryGetValue(extension, out importer);
+        RemoveObject(obj.Guid);
     }
 }
