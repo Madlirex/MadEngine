a2e09e115bf3d36d634d1a671148a80159bd1602
Madlirex <simon.buzgo@gmail.com>
2026-08-09 18:21:07 +0200

    feat(asset): add DoNotSave attribute to ignore fields when saving

--- MadEngine.Core/Asset.cs
+++ MadEngine.Core/Asset.cs
@@ -3,10 +3,15 @@
 public abstract class Asset : MadObject
 {
     public override string Name { get; set; } = "NewAsset";
+    [DoNotSave]
     public string AbsolutePath => FullDir + @"\" + Name + Extension;
+    [DoNotSave]
     public string RelativePath => RelativeDir + @"\" + Name + Extension;
+    [DoNotSave]
     public virtual string Extension => ".asset";
+    [DoNotSave]
     public string FullDir { get; set; } = Application.Directory;
+    [DoNotSave]
     public string RelativeDir => FullDir.Replace(Application.Directory, "");
 
     public Asset()
---
+++ MadEngine.Core/AssetRules.cs
@@ -0,0 +1,4 @@
+﻿namespace MadEngine.Core;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class DoNotSaveAttribute : Attribute;
