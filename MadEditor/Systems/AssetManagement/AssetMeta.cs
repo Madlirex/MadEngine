635e1bad03245038833311cb16664b20004bbca6
Madlirex <simon.buzgo@gmail.com>
2026-08-09 18:28:06 +0200

    feat(meta): add meta data

---
+++ MadEditor/Systems/AssetManagement/AssetMeta.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using MadEngine.Core;
+
+namespace MadEditor;
+
+public class AssetMeta
+{
+    public static int Version => 1;
+    public required Guid Guid;
+    public required string Name;
+    public required string RelativePath;
+    public required string Importer;
+    public Dictionary<string, JsonElement> ImportSettings = [];
+
+    public static AssetMeta Generate(Asset asset)
+    {
+        return new AssetMeta()
+        {
+            Guid = asset.Guid,
+            Name = asset.Name,
+            RelativePath = asset.RelativePath,
+            //Importer = ImporterRegistry.GetImporter(asset.GetType())
+        };
+    }
+}
