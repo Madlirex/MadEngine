59411e3cb7fe1f74433362ef138a43c8358bd140
Madlirex <simon.buzgo@gmail.com>
2026-08-10 16:29:22 +0200

    feat(serializer): add basic serializer and serializer registry logic

---
+++ MadEditor/Systems/AssetManagement/Serializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Nodes;
+
+namespace MadEditor;
+
+public interface ISerializer
+{
+    public Type Type { get; }
+    public JsonObject Serialize(object obj);
+    public object? Deserialize(JsonObject obj);
+    public void DeserializeInto(object target, JsonObject souce);
+}
+
+public abstract class Serializer<T> : ISerializer
+{
+    public Type Type => typeof(T);
+    public abstract JsonObject Serialize(T obj);
+    public abstract T Deserialize(JsonObject obj);
+    public abstract void DeserializeInto(T target, JsonObject souce);
+    
+    JsonObject ISerializer.Serialize(object obj) => Serialize((T)obj);
+
+    object? ISerializer.Deserialize(JsonObject obj) => Deserialize(obj);
+    void ISerializer.DeserializeInto(object target, JsonObject souce) => DeserializeInto((T)target, souce);
+}
---
+++ MadEditor/Systems/AssetManagement/SerializerRegistry.cs
@@ -0,0 +1,47 @@
+﻿namespace MadEditor;
+
+public static class SerializerRegistry
+{
+    public static IReadOnlyDictionary<Type, ISerializer> Serializers => _serializers;
+    private static Dictionary<Type, ISerializer> _serializers = [];
+    
+    public static ISerializer? GetSerializer(Type type)
+    {
+        Type? currentType = type;
+        while (currentType != null)
+        {
+            if (_serializers.TryGetValue(type, out ISerializer? serializer))
+                return serializer;
+            currentType = type.BaseType;
+        }
+
+        foreach (Type interfaceType in type.GetInterfaces())
+        {
+            if (_serializers.TryGetValue(type, out ISerializer? serializer))
+                return serializer;
+        }
+        return null;
+    }
+
+    public static void DiscoverSerializers()
+    {
+        _serializers.Clear();
+        var serializerTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => typeof(ISerializer)
+                .IsAssignableFrom(type) && type is { IsAbstract: false, IsInterface: false });
+
+        foreach (Type serializerType in serializerTypes)
+        {
+            RegisterSerializer(serializerType);
+        }
+    }
+    
+    public static void RegisterSerializer(Type type)
+    {
+        if (Activator.CreateInstance(type) is ISerializer serializer)
+        {
+            _serializers.TryAdd(type, serializer);
+        }
+    }
+}
