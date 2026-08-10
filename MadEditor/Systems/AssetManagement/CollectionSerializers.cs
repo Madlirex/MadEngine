76418e304b38cc82ace46e71657198af14c5ab81
Madlirex <simon.buzgo@gmail.com>
2026-08-10 17:36:55 +0200

    feat(serializer): add collection serializers

---
+++ MadEditor/Systems/AssetManagement/CollectionSerializers.cs
@@ -0,0 +1,148 @@
+﻿using System.Text.Json.Nodes;
+
+namespace MadEditor;
+
+public class ListSerializer<T> : Serializer<List<T>>
+{
+    private readonly ISerializer? _itemSerializer = SerializerRegistry.GetSerializer(typeof(T));
+
+    public override JsonNode Serialize(List<T> obj)
+    {
+        var jsonArray = new JsonArray();
+
+        if (_itemSerializer == null)
+            throw new InvalidOperationException($"No serializer found for list item type {typeof(T).Name}");
+
+        foreach (var item in obj)
+        {
+            if (item == null)
+            {
+                jsonArray.Add(JsonValue.Create<object>(null));
+            }
+            else
+            {
+                jsonArray.Add(_itemSerializer.Serialize(item));
+            }
+        }
+        return jsonArray;
+    }
+
+    public override List<T> Deserialize(JsonNode obj)
+    {
+        var list = new List<T>();
+        if (obj is not JsonArray jsonArray) return list;
+
+        if (_itemSerializer == null)
+            throw new InvalidOperationException($"No serializer found for list item type {typeof(T).Name}");
+
+        foreach (var node in jsonArray)
+        {
+            if (node == null)
+            {
+                list.Add(default!); 
+            }
+            else
+            {
+                list.Add((T)_itemSerializer.Deserialize(node)!);
+            }
+        }
+        return list;
+    }
+}
+
+public class ArraySerializer<T> : Serializer<T[]>
+{
+    private readonly ISerializer? _itemSerializer = SerializerRegistry.GetSerializer(typeof(T));
+
+    public override JsonNode Serialize(T[] obj)
+    {
+        var jsonArray = new JsonArray();
+
+        if (_itemSerializer == null)
+            throw new InvalidOperationException($"No serializer found for array element type {typeof(T).Name}");
+
+        foreach (var item in obj)
+        {
+            if (item == null)
+            {
+                jsonArray.Add(JsonValue.Create((object?)null));
+            }
+            else
+            {
+                jsonArray.Add(_itemSerializer.Serialize(item));
+            }
+        }
+        return jsonArray;
+    }
+
+    public override T[] Deserialize(JsonNode obj)
+    {
+        if (obj is not JsonArray jsonArray) return Array.Empty<T>();
+
+        if (_itemSerializer == null)
+            throw new InvalidOperationException($"No serializer found for array element type {typeof(T).Name}");
+
+        var array = new T[jsonArray.Count];
+        for (int i = 0; i < jsonArray.Count; i++)
+        {
+            var node = jsonArray[i];
+            if (node == null)
+            {
+                array[i] = default!;
+            }
+            else
+            {
+                array[i] = (T)_itemSerializer.Deserialize(node)!;
+            }
+        }
+        return array;
+    }
+}
+
+public class DictionarySerializer<TValue> : Serializer<Dictionary<string, TValue>>
+{
+    private readonly ISerializer? _valueSerializer = SerializerRegistry.GetSerializer(typeof(TValue));
+
+    public override JsonNode Serialize(Dictionary<string, TValue> obj)
+    {
+        var jsonObject = new JsonObject();
+
+        if (_valueSerializer == null)
+            throw new InvalidOperationException($"No serializer found for dictionary value type {typeof(TValue).Name}");
+
+        foreach (var pair in obj)
+        {
+            if (pair.Value == null)
+            {
+                jsonObject[pair.Key] = JsonValue.Create<object>(null);
+            }
+            else
+            {
+                jsonObject[pair.Key] = _valueSerializer.Serialize(pair.Value);
+            }
+        }
+        return jsonObject;
+    }
+
+    public override Dictionary<string, TValue> Deserialize(JsonNode obj)
+    {
+        var dictionary = new Dictionary<string, TValue>();
+        if (obj is not JsonObject jsonObject) return dictionary;
+
+        if (_valueSerializer == null)
+            throw new InvalidOperationException($"No serializer found for dictionary value type {typeof(TValue).Name}");
+
+        foreach (var pair in jsonObject)
+        {
+            if (pair.Value == null)
+            {
+                dictionary[pair.Key] = default!;
+            }
+            else
+            {
+                dictionary[pair.Key] = (TValue)_valueSerializer.Deserialize(pair.Value)!;
+            }
+        }
+        return dictionary;
+    }
+}
--- MadEditor/Systems/AssetManagement/SerializerRegistry.cs
+++ MadEditor/Systems/AssetManagement/SerializerRegistry.cs
@@ -20,28 +20,69 @@ public static class SerializerRegistry
             if (_serializers.TryGetValue(interfaceType, out ISerializer? serializer))
                 return serializer;
         }
-        return null;
+        
+        return TryGenerateDynamicSerializer(type);
     }
 
+    public static ISerializer? TryGenerateDynamicSerializer(Type type)
+    {
+        if (type.IsArray)
+        {
+            Type elementType = type.GetElementType()!;
+            Type arraySerializerType = typeof(ArraySerializer<>).MakeGenericType(elementType);
+            return CreateSerializer(arraySerializerType);
+        }
+        
+        if (type.IsGenericType)
+        {
+            Type genericDefinition = type.GetGenericTypeDefinition();
+
+            if (genericDefinition == typeof(List<>))
+            {
+                Type itemType = type.GetGenericArguments()[0];
+                Type listSerializerType = typeof(ListSerializer<>).MakeGenericType(itemType);
+                return CreateSerializer(listSerializerType);
+            }
+            
+            if (genericDefinition == typeof(Dictionary<,>))
+            {
+                Type[] genericArgs = type.GetGenericArguments();
+                if (genericArgs[0] == typeof(string))
+                {
+                    Type valueType = genericArgs[1];
+                    Type dictSerializerType = typeof(DictionarySerializer<>).MakeGenericType(valueType);
+                    return CreateSerializer(dictSerializerType);
+                }
+            }
+        }
+
+        return null;
+    }
+    
     public static void DiscoverSerializers()
     {
         _serializers.Clear();
         var serializerTypes = AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(assembly => assembly.GetTypes())
             .Where(type => typeof(ISerializer)
-                .IsAssignableFrom(type) && type is { IsAbstract: false, IsInterface: false });
+                .IsAssignableFrom(type) && type is { IsAbstract: false, IsInterface: false, IsGenericTypeDefinition: false });
 
         foreach (Type serializerType in serializerTypes)
         {
-            RegisterSerializer(serializerType);
+            CreateSerializer(serializerType);
         }
     }
-    
-    public static void RegisterSerializer(Type type)
+
+    public static ISerializer? CreateSerializer(Type type)
     {
+        if (type.IsGenericType) return null;
+        
         if (Activator.CreateInstance(type) is ISerializer serializer)
         {
-            _serializers.TryAdd(type, serializer);
+            _serializers.TryAdd(serializer.Type, serializer);
+            return serializer;
         }
+
+        return null;
     }
 }
