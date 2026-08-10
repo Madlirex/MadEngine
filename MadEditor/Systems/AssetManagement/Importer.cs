f745218c94f71dad854a58132ae680988be7ba52
Madlirex <simon.buzgo@gmail.com>
2026-08-10 16:19:06 +0200

    feat(asset): add importer and importer registry logic

---
+++ MadEditor/Systems/AssetManagement/Importer.cs
@@ -0,0 +1,33 @@
+﻿using MadEngine.Core;
+
+namespace MadEditor;
+
+public interface IAssetImporter
+{
+    public Type Type { get; }
+    public string Name => "Importer";
+    public string Extension => ".asset";
+
+    public Asset Initialize(string path);
+    public Asset Initialize(AssetMeta meta);
+    public Asset Import(string path);
+    
+    public void Save(Asset asset);
+}
+
+public abstract class Importer<T> : IAssetImporter where T : Asset
+{
+    public Type Type => typeof(T);
+    public abstract T Initialize(string path);
+    public abstract T Initialize(AssetMeta meta);
+    public abstract T Import(string path);
+    
+    public abstract void Save(T asset);
+
+    Asset IAssetImporter.Initialize(string path) => Initialize(path);
+    Asset IAssetImporter.Initialize(AssetMeta meta) => Initialize(meta);
+    
+    Asset IAssetImporter.Import(string path) => Import(path);
+    
+    void IAssetImporter.Save(Asset asset) => Save((T)asset);
+}
---
+++ MadEditor/Systems/AssetManagement/ImporterRegistry.cs
@@ -0,0 +1,47 @@
+﻿namespace MadEditor;
+
+public static class ImporterRegistry
+{
+    public static IReadOnlyDictionary<Type, IAssetImporter> Importers => _importers;
+    private static Dictionary<Type, IAssetImporter> _importers = [];
+    
+    public static IAssetImporter? GetImporter(Type type)
+    {
+        Type? currentType = type;
+        while (currentType != null)
+        {
+            if (_importers.TryGetValue(type, out IAssetImporter? importer))
+                return importer;
+            currentType = type.BaseType;
+        }
+
+        foreach (Type interfaceType in type.GetInterfaces())
+        {
+            if (_importers.TryGetValue(type, out IAssetImporter? importer))
+                return importer;
+        }
+        return null;
+    }
+
+    public static void DiscoverImporters()
+    {
+        _importers.Clear();
+        var importerTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => typeof(IAssetImporter)
+                .IsAssignableFrom(type) && type is { IsAbstract: false, IsInterface: false });
+
+        foreach (Type importerType in importerTypes)
+        {
+            RegisterImporter(importerType);
+        }
+    }
+    
+    public static void RegisterImporter(Type type)
+    {
+        if (Activator.CreateInstance(type) is IAssetImporter importer)
+        {
+            _importers.TryAdd(type, importer);
+        }
+    }
+}
