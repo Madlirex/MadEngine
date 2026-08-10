17a3280fe87577324ff13dafc91999a9847c011d
Madlirex <simon.buzgo@gmail.com>
2026-08-10 16:37:54 +0200

    fix(serializer): change JsonObject to JsonNode

--- MadEditor/Systems/AssetManagement/Serializer.cs
+++ MadEditor/Systems/AssetManagement/Serializer.cs
@@ -5,20 +5,20 @@ namespace MadEditor;
 public interface ISerializer
 {
     public Type Type { get; }
-    public JsonObject Serialize(object obj);
-    public object? Deserialize(JsonObject obj);
-    public void DeserializeInto(object target, JsonObject souce);
+    public JsonNode Serialize(object obj);
+    public object? Deserialize(JsonNode obj);
+    public void DeserializeInto(object target, JsonNode source);
 }
 
 public abstract class Serializer<T> : ISerializer
 {
     public Type Type => typeof(T);
-    public abstract JsonObject Serialize(T obj);
-    public abstract T Deserialize(JsonObject obj);
-    public abstract void DeserializeInto(T target, JsonObject souce);
+    public abstract JsonNode Serialize(T obj);
+    public abstract T Deserialize(JsonNode obj);
+    public abstract void DeserializeInto(T target, JsonNode source);
     
-    JsonObject ISerializer.Serialize(object obj) => Serialize((T)obj);
+    JsonNode ISerializer.Serialize(object obj) => Serialize((T)obj);
 
-    object? ISerializer.Deserialize(JsonObject obj) => Deserialize(obj);
-    void ISerializer.DeserializeInto(object target, JsonObject souce) => DeserializeInto((T)target, souce);
+    object? ISerializer.Deserialize(JsonNode obj) => Deserialize(obj);
+    void ISerializer.DeserializeInto(object target, JsonNode source) => DeserializeInto((T)target, source);
 }
