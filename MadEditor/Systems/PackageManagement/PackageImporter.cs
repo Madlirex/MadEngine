c795f2871e729b6c2ee31d21d2bdd168cb0e7b1b
Madlirex <simon.buzgo@gmail.com>
2026-08-12 15:14:32 +0200

    feat(package-importer): basic functionality

---
+++ MadEditor/Systems/PackageManagement/PackageImporter.cs
@@ -0,0 +1,44 @@
+﻿using MadEngine.Core;
+
+namespace MadEditor.PackageManagement;
+
+public static class PackageImporter
+{
+    public static void ImportPackage(string packagePath, PackageMeta packageMeta)
+    {
+        CopyDirectory(packagePath, Application.PackagesPath + @$"\{packageMeta.Name}\");
+    }
+
+    public static void RemovePackage(PackageMeta packageMeta)
+    {
+        string targetFilePath = Path.Combine(Application.PackagesPath, packageMeta.Name);
+        if (!Directory.Exists(targetFilePath)) return;
+        
+        Directory.Delete(targetFilePath, true);
+    }
+    
+    public static void CopyDirectory(string sourceDir, string destinationDir, bool overwrite = true)
+    {
+        var dir = new DirectoryInfo(sourceDir);
+
+        if (!dir.Exists)
+        {
+            throw new DirectoryNotFoundException($"Source directory does not exist: {sourceDir}");
+        }
+        
+        Directory.CreateDirectory(destinationDir);
+        
+        foreach (FileInfo file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            file.CopyTo(targetFilePath, overwrite);
+        }
+        
+        foreach (DirectoryInfo subDir in dir.GetDirectories())
+        {
+            string targetSubDirPath = Path.Combine(destinationDir, subDir.Name);
+            
+            CopyDirectory(subDir.FullName, targetSubDirPath, overwrite);
+        }
+    }
+}
