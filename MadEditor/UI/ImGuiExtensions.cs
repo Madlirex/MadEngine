8dff4cc05ecafa0b156f60547443dc06a89343fd
Madlirex <simon.buzgo@gmail.com>
2026-08-15 17:49:22 +0200

    feat(imgui): add extensions for selectable text and disabled text

---
+++ MadEditor/UI/ImGuiExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using ImGuiNET;
+
+namespace MadEditor;
+
+public static class ImGuiEx
+{
+    public static void SelectableText(string text)
+    {
+        ImGui.PushStyleColor(ImGuiCol.Border, Vector4.Zero);
+        
+        float textWidth = ImGui.CalcTextSize(text).X + ImGui.GetStyle().FramePadding.X * 2.0f;
+        ImGui.SetNextItemWidth(textWidth);
+        
+        ImGui.InputText($"##SelText_{text.GetHashCode()}", ref text, (uint)text.Length + 1, ImGuiInputTextFlags.ReadOnly);
+
+        ImGui.PopStyleColor(2);
+    }
+    
+    public static void SelectableTextDisabled(string text)
+    {
+        Vector4 disabledTextColor = ImGui.GetStyle().Colors[(int)ImGuiCol.TextDisabled];
+        
+        ImGui.PushStyleColor(ImGuiCol.Text, disabledTextColor);
+        
+        SelectableText(text);
+
+        ImGui.PopStyleColor();
+    }
+}
--- MadEditor/UI/UIDrawers/PanelDrawers/PanelDrawer.cs
+++ MadEditor/UI/UIDrawers/PanelDrawers/PanelDrawer.cs
@@ -6,7 +6,7 @@ public abstract class PanelDrawer
 {
     public Guid Guid { get; } = Guid.NewGuid();
     public abstract PanelRegion PanelRegion { get; set; }
-    public virtual Vector2 MinSize { get; } = new Vector2(500, 400);
+    public virtual Vector2 MinSize { get; } = new(500, 400);
     public abstract void Draw(EditorUIContext context);
 
     public override string ToString()
