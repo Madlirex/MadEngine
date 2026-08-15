0b010096a2107ecf54f3eb56503d5a3f48005ce4
Madlirex <simon.buzgo@gmail.com>
2026-08-15 16:51:34 +0200

    feat(panels): the same I restored the wrong file, so add package manager UI

--- MadEditor/UI/UIDrawers/PanelDrawers/PanelDrawer.cs
+++ MadEditor/UI/UIDrawers/PanelDrawers/PanelDrawer.cs
@@ -5,7 +5,6 @@ namespace MadEditor;
 public abstract class PanelDrawer
 {
     public Guid Guid { get; } = Guid.NewGuid();
-    public abstract string Name { get; }
     public abstract PanelRegion PanelRegion { get; set; }
     public abstract void Draw(EditorUIContext context);
 
