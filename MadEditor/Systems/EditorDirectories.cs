a1c1d866f288e44a959c52593ec535603197890f
Madlirex <simon.buzgo@gmail.com>
2026-08-16 16:29:20 +0200

    feat(directories): add basic directories

---
+++ MadEditor/Systems/EditorDirectories.cs
@@ -0,0 +1,11 @@
+﻿namespace MadEditor;
+
+public static class EditorDirectories
+{
+    public static string ExecutablePath { get; } = AppContext.BaseDirectory;
+    
+    private static string DataPathBase { get; } = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    public static string PersistentDataPath { get; } = Path.Combine(DataPathBase, "MadlirexStudios", "MadEditor");
+
+    public static string ConfigPath { get; } = Path.Combine(PersistentDataPath, "Config");
+}
