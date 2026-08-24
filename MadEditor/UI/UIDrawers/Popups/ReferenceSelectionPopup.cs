c6c4984025076d6013cda9b0d86e0b893189149b
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-08-24 17:28:26 +0200

    feat(reference-popup): create basic class

---
+++ MadEditor/UI/UIDrawers/Popups/ReferenceSelectionPopup.cs
@@ -0,0 +1,13 @@
+﻿using MadEngine.Core;
+
+namespace MadEditor;
+
+public class ReferenceSelectionPopup : Popup
+{
+    public Type Type = typeof(MadObject);
+    
+    protected override void Body(EditorUIContext context)
+    {
+        
+    }
+}
