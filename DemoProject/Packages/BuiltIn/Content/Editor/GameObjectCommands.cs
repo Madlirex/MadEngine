56dddf8db464326b55bf0928aae062a450b1cb0b
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-08-25 17:05:15 +0200

    feat(commands): basic gameobject commands

---
+++ DemoProject/Packages/BuiltIn/Content/Editor/GameObjectCommands.cs
@@ -0,0 +1,34 @@
+﻿using MadEngine.Core;
+
+namespace MadEditor;
+
+public class AddEmptyCommand : PopupCommand<GameObject>
+{
+    public override string Path => "Add Empty";
+    public override void Execute(GameObject target)
+    {
+        GameObject go = new GameObject
+        {
+            Transform =
+            {
+                Parent = target.Transform
+            }
+        };
+    }
+}
+
+public class AddEmptyParentCommand : PopupCommand<GameObject>
+{
+    public override string Path => "Add Empty Parent";
+    public override void Execute(GameObject target)
+    {
+        GameObject go = new GameObject()
+        {
+            Transform =
+            {
+                Parent = target.Transform.Parent
+            }
+        };
+        target.Transform.Parent = go.Transform;
+    }
+}
--- MadEditor/UI/UIDrawers/Popups/HierarchyPopup.cs
+++ MadEditor/UI/UIDrawers/Popups/HierarchyPopup.cs
@@ -12,12 +12,3 @@ public class HierarchyPopup : Popup
         PopupCommandsRegistry.RenderContextMenu(context.RightClicked);
     }
 }
-
-public class CreateCommand : PopupCommand<Shader>
-{
-    public override string Path => "Object/Create";
-    public override void Execute(Shader target)
-    {
-        
-    }
-}
