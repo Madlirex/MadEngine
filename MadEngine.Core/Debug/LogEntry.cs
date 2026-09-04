41f392dde73c229987cdf2ac967203ec78ec1c40
Madlirex <simon.buzgo@gmail.com>
2026-09-04 18:03:30 +0200

    feat(debug): create basic debug

---
+++ MadEngine.Core/Debug/LogEntry.cs
@@ -0,0 +1,26 @@
+﻿namespace MadEngine.Core;
+
+public enum LogType
+{
+    Info,
+    Warning,
+    Error
+};
+
+public class LogEntry
+{
+    public string Message { get; init; }
+    public string StackTrace { get; init; }
+    public LogType Type { get; init; }
+    public DateTime Timestamp { get; init; }
+    public int Count { get; set; }
+
+    public LogEntry(string message, string stackTrace, LogType type)
+    {
+        Message = message;
+        StackTrace = stackTrace;
+        Type = type;
+        Timestamp = DateTime.Now;
+        Count = 1;
+    }
+}
