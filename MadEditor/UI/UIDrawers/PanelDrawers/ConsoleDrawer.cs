cfa836fe398dd5987403c93e284273baf987e236
Madlirex <simon.buzgo@gmail.com>
2026-09-04 18:05:30 +0200

    feat(panel-region): make it optional for making panel drawers

---
+++ MadEditor/UI/UIDrawers/PanelDrawers/ConsoleDrawer.cs
@@ -0,0 +1,13 @@
+﻿using MadEngine.Core;
+
+namespace MadEditor;
+
+[CustomName("Console")]
+public class ConsoleDrawer : PanelDrawer
+{
+    public override PanelRegion PanelRegion { get; set; } = PanelRegion.Bottom;
+    public override void Draw(EditorUIContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
