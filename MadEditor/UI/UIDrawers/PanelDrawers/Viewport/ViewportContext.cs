5f2d35011e1fbfaa5e38678a3a1641483df51e52
Madlirex <simon.buzgo@gmail.com>
2026-09-06 19:39:05 +0200

    feat(viewport-context): create context

---
+++ MadEditor/UI/UIDrawers/PanelDrawers/Viewport/ViewportContext.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+using MadEngine.Core;
+
+namespace MadEditor;
+
+public class ViewportContext : IDisposable
+{
+    public string Id { get; }
+    public GameObject CameraObject
+    {
+        get
+        {
+            if (_camera != null) return _camera;
+            _camera = new GameObject();
+            _cameraComponent = new Camera();
+            _camera.AddComponentUnsafe(_cameraComponent);
+            return _camera;
+        }
+    }
+
+    private GameObject? _camera;
+
+    public Camera CameraComponent
+    {
+        get
+        {
+            if(_cameraComponent != null) return _cameraComponent;
+            _camera = null;
+            return CameraObject.GetComponent<Camera>()!;
+        }
+    }
+
+    private Camera? _cameraComponent;
+    public SceneFramebuffer Framebuffer { get; }
+    public Vector2 Size { get; set; } = Vector2.Zero;
+
+    public ViewportContext(string id, int initialWidth, int initialHeight)
+    {
+        Id = id;
+        
+        _camera = new GameObject { Name = $"EditorCamera_{id}" };
+        _cameraComponent = new Camera();
+        _camera.AddComponentUnsafe(_cameraComponent);
+
+        _cameraComponent.Width = initialWidth;
+        _cameraComponent.Height = initialHeight;
+        Framebuffer = new SceneFramebuffer(initialWidth, initialHeight);
+    }
+
+    public void Dispose()
+    {
+        Framebuffer.Dispose();
+        _camera?.Destroy();
+    }
+}
