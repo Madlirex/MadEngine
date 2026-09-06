510e88a1de9bfcce1482ceb298012b205da10632
Madlirex <simon.buzgo@gmail.com>
2026-09-06 22:00:13 +0200

    fix(recompilation): selected object getting unselected 8100(6400)

--- MadEditor/Systems/EditorCommands/RecompilationCommand.cs
+++ MadEditor/Systems/EditorCommands/RecompilationCommand.cs
@@ -8,8 +8,14 @@ public class RecompilationCommand : IEditorCommand
     public void Execute(object target)
     {
         DateTime start = DateTime.Now;
+
+        Guid? selectedGuid = EditorUI.UiContext.Selected?.Guid;
+        
         AssetManager.RecompileScripts();
         DateTime end = DateTime.Now;
+
+        if (selectedGuid.HasValue)
+            EditorUI.UiContext.Selected = AssetRegistry.GetObject(selectedGuid.Value);
         
         Debug.Log($"Compilation finished in: {end - start}");
         
