bdbc7bf799a9bd440eb734c0c9b06d4582e0607c
Madlirex <simon.buzgo@gmail.com>
2026-09-13 17:47:22 +0200

    feat(folder-asset): saving

--- DemoProject/Packages/BuiltIn/Content/Editor/Importers/FolderAssetImporter.cs
+++ DemoProject/Packages/BuiltIn/Content/Editor/Importers/FolderAssetImporter.cs
@@ -6,4 +6,9 @@ public class FolderAssetImporter : EmptyAssetImporter<FolderAsset>
 {
     public override string Name => "FolderImporter";
     public override string[] Extensions => [""];
+
+    public override void Save(FolderAsset asset)
+    {
+        if(!Directory.Exists(asset.AbsolutePath)) Directory.CreateDirectory(asset.AbsolutePath);
+    }
 }
